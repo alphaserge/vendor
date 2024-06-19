0a2ec8be9c24c09f1f84cefe7fff6c46c4210043
alphaserge <alphaserge@yandex.ru>
2024-06-19 12:04:38 +0300

    Added Photo Helper class

---
+++ backend/webapi/Code/PhotoHelper.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace chiffon_back.Code
+{
+    public class PhotoHelper
+    {
+        public static string[] GetPhotoUuids(string? uuids)
+        {
+            if (String.IsNullOrWhiteSpace(uuids))
+                return Array.Empty<string>();
+            
+             return uuids!.Split(',');
+        }
+    }
+}
