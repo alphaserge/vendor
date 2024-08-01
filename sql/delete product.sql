062e207698e130684f68e316aa63b8b652d3ff3f
elizarovsa <elizarov.sa@mail.ru>
2024-08-01 20:22:32 +0300

    SQL for delete group of products: ..<id<..

--- sql/delete product.sql
+++ sql/delete product.sql
@@ -1,6 +1,10 @@
 
-declare @id int
-set @id = 90
+--declare @id int
+--set @id = 90
+DECLARE @id int = 91
+
+WHILE @id < 113
+BEGIN
 
 delete from ColorVariantsInColors where ColorVariantId in (select Id FROM [chiffon].[dbo].[ColorVariants] where ProductId in (@id))
 delete   FROM [chiffon].[dbo].[ColorVariants] where ProductId in (@id)
@@ -9,5 +13,11 @@ delete from ProductsInSeasons where ProductId in (@id)
 delete from ProductsInOverWorkTypes where ProductId in (@id)
 
 delete FROM [chiffon].[dbo].[Products] where Id in (@id)
+
+    /* do some work */
+
+    SET @id = @id + 1
+
+END
 
 SELECT * FROM [chiffon].[dbo].[Products]
