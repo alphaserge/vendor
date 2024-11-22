c84f23ab16d0b9495038f4bae86fdc885e3e11ab
elizarovsa <elizarov.sa@mail.ru>
2024-11-22 12:28:59 +0300

    Header main section

--- front/src/pages/home/mainsection.js
+++ front/src/pages/home/mainsection.js
@@ -19,34 +19,36 @@ useEffect(() => {
   }, []);
 
   return (
-    <Box sx={{ backgroundColor: "#eeede8", alignContent: "center" }} style={{ height: "100px" }}  >
-      {/* <Box style={{ height: "100px" }} > */}
+    // #eeede8  efa29a
+    <Box className="main-section">
+    <Box sx={{ backgroundColor: "#f78997", color: "#fff", alignContent: "center", height: "44px" }} >
+      <Grid container spacing={2} >
+      <Grid item xs={12} md={3} key={"mainsect-left"} sx={{ justifyItems : "center" }} >
+        <p>-</p>
+      </Grid>
+      <Grid item xs={12} md={6} key={"mainsect-center"} sx={{ justifyItems : "center" }}  >
+        <p>Sign up for our newsletter for 15% off</p>
+      </Grid>
+      <Grid item xs={12} md={3} key={"mainsect-right"} sx={{ justifyItems : "center" }} >
+        <p>-</p>
+      </Grid>
+      </Grid>
+    </Box>
 
+    <Box sx={{ backgroundColor: "#fff", color: "#424242", alignContent: "center" }} style={{ height: "100px" }}  >
       <Grid container spacing={2} >
       <Grid item xs={12} md={3} key={"mainsect-left"} sx={{ justifyItems : "center" }} >
         <p>left</p>
       </Grid>
-      <Grid item xs={12} md={6} key={"mainsect-center"} sx={{ justifyItems : "center" }} className="site-logo" >
-        {/* <p>Angelica fabric market</p> */}
-        <Box
-            component="img"
-            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
-            image="/afm.png"
-            alt={""}
-          />
+      <Grid item xs={12} md={6} key={"mainsect-center"} sx={{ justifyItems : "center" }}  >
+        <p className="site-logo">Angelica fabric market</p>
       </Grid>
       <Grid item xs={12} md={3} key={"mainsect-right"} sx={{ justifyItems : "center" }} >
         <p>right</p>
       </Grid>
       </Grid>
-      {/*<Grid container spacing={2} >
-        <Grid item xs={12} md={6} key={"itemprod-"+index} >
-          <ItemProduct data={data} index={index} />
-        </Grid>
-      </Grid> */}
-
-      {/* </Box> */}
-      </Box>
+    </Box>
+    </Box>
   );
 }
 
