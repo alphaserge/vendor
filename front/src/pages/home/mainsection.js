50bdea24a6ffcf03605441478f8d72a27f28326a
elizarovsa <elizarov.sa@mail.ru>
2024-11-25 13:40:21 +0300

    Find begin

--- front/src/pages/home/mainsection.js
+++ front/src/pages/home/mainsection.js
@@ -2,8 +2,16 @@ import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import { Icon } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+
 import { APPEARANCE } from '../../appearance';
 import { Height } from "@mui/icons-material";
+
+const findBoxStyle = { width: "calc(100% - 80px)", border: "none" }
+const findTextStyle = { width: "100%", borderRadius : "8px", border: "2px solid #18515e" }
 
 function FabricIcon(props) {
   return (
@@ -14,10 +22,28 @@ function FabricIcon(props) {
 
 export default function MainSection(props) {
 
+  const [search, setSearch] = useState("")
+
+  const styles = theme => ({
+    textField: {
+        width: '90%',
+        marginLeft: 'auto',
+        marginRight: 'auto',            
+        paddingBottom: 0,
+        marginTop: 0,
+        fontWeight: 500,
+        borderRadius: '20px'
+    },
+    input: {
+        color: 'white'
+    }
+  });
+
 useEffect(() => {
     //console.log(props.title)
   }, []);
 
+  const { classes } = props;
   return (
     // #eeede8  efa29a
     <Box className="main-section">
@@ -44,7 +70,28 @@ useEffect(() => {
         <p className="site-logo">Angelica fabric market</p>
       </Grid>
       <Grid item xs={12} md={3} key={"mainsect-right"} sx={{ justifyItems : "center" }} >
-        <p>right</p>
+      <Box style={findBoxStyle}>
+          <TextField
+                margin="normal"
+                size="small" 
+                id="search-value"
+                label="Find products"
+                name="search"
+                value={search}
+                className={styles.textField}
+                onChange={ev => props.searchProducts(ev.target.value)}
+                InputProps={{
+                  className: styles.input,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton>
+                        <SearchIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  )
+                }}
+              />
+          </Box>
       </Grid>
       </Grid>
     </Box>
