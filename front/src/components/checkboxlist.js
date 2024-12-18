740722035a134e704b2b9f8f43cdf84c6cb6714e
elizarovsa <elizarov.sa@mail.ru>
2024-12-18 18:14:04 +0300

    work on work

---
+++ front/src/components/checkboxlist.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import { Accordion, AccordionSummary, AccordionDetails, InputLabel } from "@mui/material"
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import Typography from '@mui/material/Typography';
+
+export default function CheckboxList(props) {
+  const [checked, setChecked] = React.useState([0]);
+
+  const handleToggle = (value) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  return (
+    <Box sx={{ border: "none", width: "100%", paddingTop: "10px" }} >
+    <Accordion className="header-menu" defaultExpanded={props.expanded? props.expanded : false} disableGutters sx={{ boxShadow: "none" }} >
+
+    <AccordionSummary expandIcon={<ExpandMoreIcon sx={{margin: 0}} />} sx={{ maxWidth: "744px", margin: "0 auto !important", padding: "0 3px", flexDirection: "row-reverse" }} >
+      <Typography sx={{ width: "100%", fontWeight: "bold", fontSize: "11pt", margin: 0 }}> &nbsp;&nbsp;{props.title}</Typography>
+    </AccordionSummary>
+
+    <AccordionDetails sx={{ maxWidth: "744px", margin: "0 auto", padding: "0 0px", overflowX: "hidden", overflowY: "scroll" }}>
+    <List sx={{ width: '100%', maxWidth: 360, height: props.height? props.height : 210, bgcolor: 'background.paper' }}>
+      {props.list.map((item) => {
+        const labelId = `checkbox-list-label-${item.key}`;
+
+        return (
+          <ListItem
+            key={item.key}
+            disablePadding
+          >
+            <ListItemButton role={undefined} onClick={handleToggle(item.key)} sx={{ padding: 0, margin: 0 }}>
+              <ListItemIcon sx={{ padding: 0, margin: 0 }}>
+                <Checkbox
+                  edge="start"
+                  checked={checked.includes(item.key)}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ 'aria-labelledby': labelId }}
+                  sx={{ padding: 0, margin: 0 }}
+                />
+              </ListItemIcon>
+              <ListItemText id={labelId} primary={item.name} sx={{ padding: "2px 0px", margin: 0 }}/>
+            </ListItemButton>
+          </ListItem>
+        );
+      })}
+    </List>
+    </AccordionDetails>
+    </Accordion>
+    </Box>
+  );
+}
