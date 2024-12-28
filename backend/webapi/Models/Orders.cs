9ed8462915455c772fcc275ef487c8918fd8624e
elizarovsa <elizarov.sa@mail.ru>
2024-12-28 13:03:12 +0300

    Orders table - cs

--- backend/webapi/Models/Orders.cs
+++ backend/webapi/Models/Orders.cs
@@ -1,21 +1,9 @@
 ﻿namespace chiffon_back.Models
 {
-
-    public class PostColorVariant
-    {
-        public int? ColorNo { get; set; }
-        public int[]? ColorIds { get; set; }
-        public int? ProductId { get; set; }
-        public int? ColorVariantId { get; set; }
-        public string? Uuid { get; set; }
-        public decimal? Quantity { get; set; }
-
-    }
-
-    public class ColorVariant
+    public class Order
     {
         public int Id { get; set; }
-        public int ProductId { get; set; }
+        public int VendorId { get; set; }
         public string? Uuid { get; set; }
         public int Num { get; set; }
         public decimal? Quantity { get; set; }
